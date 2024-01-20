0c67ef71a67b055f0219a28e8ad6c3a402d9b25c
Openaicodexguy <116284467+Openaicodexguy@users.noreply.github.com>
2024-01-20 13:49:07 +0200

    Update common.js

--- src/common.js
+++ src/common.js
@@ -1,18 +1,11 @@
 export const config = {
     serverAddresses: [
-        "wss://computernewb.com/collab-vm/vm0",
-        "wss://computernewb.com/collab-vm/vm1",
-        "wss://computernewb.com/collab-vm/vm2",
-        "wss://computernewb.com/collab-vm/vm3",
-        "wss://computernewb.com/collab-vm/vm4",
-        "wss://computernewb.com/collab-vm/vm5",
-        "wss://computernewb.com/collab-vm/vm6",
-        "wss://computernewb.com/collab-vm/vm7",
-        "wss://computernewb.com/collab-vm/vm8",
-	"wss://computernewb.com/collab-vm/vm9",
-	"wss://computernewb.com/collab-vm/eventvm",
+        "wss://6004-openaicodexguy-netvm-7yrwp1it334.ws-eu107.gitpod.io", 
+        "wss://6005-openaicodexguy-netvm-7yrwp1it334.ws-eu107.gitpod.io",
+        "wss://6006-openaicodexguy-netvm-7yrwp1it334.ws-eu107.gitpod.io",
+        "wss://6007-openaicodexguy-netvm-7yrwp1it334.ws-eu107.gitpod.io"
     ],
-    chatSound: "https://computernewb.com/collab-vm/notify.ogg",
+    chatSound: "",
     // What XSS implementation the server uses
     // 0: No XSS (cvm1.2.11)
     // 1: Internal fork style (cvm1.ts, global opcode 21)
